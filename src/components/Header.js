30bf0e36fa2f51831ee229e28482d853a9c2a73c
A1doranM <vaselisk1997@gmail.com>
2019-08-01 17:26:02 +0300

    Initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Header = () =>{
+    return (
+        <header className='header'><img src='logo.svg' alt='header pic 1'/></header>
+    )
+};
+
+export default Header;
---
+++ src/components/Profile.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Profile = () =>{
+    return (
+        <div className='content'>
+            <div>
+                <img src='https://i.redd.it/ihoxdlxnodpy.jpg' alt='content pic 1'/>
+            </div>
+            <div> ava + discrip </div>
+            <div> my posts
+                <div>
+                    post 1
+                </div>
+                <div>
+                    post 2
+                </div>
+            </div>
+        </div>
+    )
+};
+
+export default Profile;
